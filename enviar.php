17b54c75e7288a05f8222f0a89ce231ec56d3654
Bardark <ren_rbk@hotmail.es>
2015-11-01 16:15:22 -0600

    nuevo

---
+++ enviar.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+</html>
+<?php
+	$nombre = $_POST["nombreContacto"];
+	$email = $_POST["correoContacto"];
+	$mensaje = $_POST["mensajeContacto"];
+	    
+	mail("disiin@hotmail.com", $nombre, $mensaje, $email); 
+                    echo '<script language="javascript" type="text/javascript">
+                        alert("¡¡Comentario enviado con éxito!!" );
+                        document.location.href="index.html"
+                    </script>'  
+?>
