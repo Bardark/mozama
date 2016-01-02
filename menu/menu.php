ff6b5a0ae203a794511cc0e102ed960b33e51665
Bardark <ren_rbk@hotmail.es>
2016-01-02 13:21:04 -0600

    cambio de logo nav

---
+++ menu/menu.php
@@ -0,0 +1,12 @@
+
+		<header class="navbar navbar-default navbar-fixed-top" role="navigation" id="barraNav">
+			<nav>
+				<ul>
+					<li><a class="page-scroll" href="#servicios">Servicios</a></li>
+					<li><a class="page-scroll" href="#portafolio">Portafolio</a></li>
+					<li><a class="page-scroll" href="#contacto">Contacto</a></li>
+				</ul>
+			</nav>
+			<img src="img/logo.png" class="logo" align="left">								
+		</header>
+	
