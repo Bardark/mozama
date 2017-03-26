111cdfc0722b7c2fd8023b27ece4a89e7405f64b
asus <asus@asus-x.lan>
2017-03-26 15:53:42 -0600

    pagina para app

---
+++ HexaText/hexaText.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="author" content="MOZAMA" />
+  <link rel="author" href="../humans.txt" />
+  <link rel="icon" type="../image/png" href="./favicon.png"/>
+
+  <title>MOZAMA</title>
+
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/estilo2.css" rel="stylesheet">
+  <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+
+<body data-spy="scroll" id="page-top" class="fondoApp">
+  <?php include ("../secciones/menuApp.php"); ?>
+
+
+  <div class="container" id="divHexa">
+
+    <br><br>
+    <div class="row" >
+      <div class="col-md-12 text-center">
+        <h1 class=" tipografiaPrecursive">Aplicaciones móviles</h1>
+      </div>
+    </div>
+
+    <br><br><br>
+    <div class="row" class="paddingApp">
+      <div class="col-md-12 text-center">
+          <img src="../img/ic_launcher.png" class="imgApp" alt="">
+          <h2 class=" tipografiaPrecursive">HexaText - Android</h2>
+      </div>
+    </div>
+    <br><br><br>
+
+    <div class="row">
+      <div class="col-md-6 ">
+        <div class="well">
+          <p class="parrafo text-justify">
+            <strong>HexaText</strong> es una aplicación de <strong>cifrado</strong> que tiene como objetivo principal, brindar confidencialidad y privacidad en la informaci&oacuten a los usuarios de dispositivos m&oacuteviles. Es decir, s&oacutelo permite el conocimiento de la informaci&oacuten a quien est&eacute autorizado.
+          </p>
+        </div>
+      </div>
+      <div class="col-md-6 ">
+        <div class="well">
+          <p class="parrafo text-justify">
+            Con <strong>HexaText</strong> podr&aacutes <strong>ocultar, guardar o compartir texto </strong> por medio de cualquier aplicaci&oacuten que procese texto (mensajería instantanea de texto, correo electrónico, entre otros).  Solo las personas que conozcan la clave utilizada al cifrar el texto y posean HexaText podr&aacuten descifrar el texto.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="well">
+          <p class="parrafo text-justify">
+            Para cubrir la vulnerabilidad de seguridad en la informaci&oacuten, <strong>HexaText</strong> implementa el algoritmo de cifrado sim&eacutetrico <strong>AES</strong> (Advanced Encryption Standard), compatible con el est&aacutendar propuesto por la <strong>NIST</strong> (National Institute of Standards and Technology), utilizando una clave de 128 bits (16 caracteres) definida por el usuario para cumplir el proceso de cifrado.
+          </p>
+        </div>
+
+      </div>
+    </div>
+
+
+    <br><br>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h4 class="tipografiaPrecursive3">Funcionamiento</h4>
+        <h3 class="tipografiaPrecursive3">CIFRAR TEXTO</h3>
+      </div>
+    </div>
+    <br><br>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="well">
+
+          <ul class="parrafo text-justify" >
+                 <li> Ingresar una clave de 16 caracteres como m&aacuteximo (ser&aacute utilizada posteriormente para el proceso de descifrado)
+                 </li>
+                 <li> El &aacuterea de texto permite copiar o ingresar culaquier tipo de texto a ser encriptado
+                 </li>
+                 <li> Click en el bot&oacuten flotante de encriptar
+                 </li>
+                 <li> Aparecer&aacute el texto cifrado, con las opciones de compartir por medio de las redes sociales m&aacutes utilizadas o bien almacenarlo en el dispositivo m&oacutevil
+                 </li><br>
+          </ul>
+
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <img src="imagenes/img001.png" class="imgTutorial">
+                </div>
+                <div class="col-md-6 text-center">
+                    <img src="imagenes/img002.png" class="imgTutorial">
+                </div>
+              </div>
+
+
+                    <br><br>
+                 <h4 class="tipografiaPrecursive2">Copartir Texto Cifrado </h4>
+                <ul class="parrafo text-justify" >
+                      <br>
+                      <li>Pulsar sobre el primer icono del men&uacute superior derecho</li>
+                    <li>Seleccionar la aplicaci&oacuten por medio de la cual se compartir&aacute el texto cifrado</li>
+
+                </ul>
+                    <br>
+
+                  <h4 class="tipografiaPrecursive2">Guardar Texto Cifrado </h4>
+                  <ul class="parrafo text-justify">
+                      <br>
+                      <li>Pulsar sobre los tres puntos del men&uacute superior derecho</li>
+                      <li>Ingresar un t&iacutetulo para poder guardar el texto</li>
+                      <li>Click en el bot&oacuten superior derecho de aceptar y se mostrar&acute
+                       la lista de textos almacenados en el m&oacutevil
+                      </li><br>
+                    </ul>
+
+
+                    <div class="row">
+                      <div class="col-md-6 text-center">
+                          <img src="imagenes/img003.png" class="imgTutorial">
+                      </div>
+                      <div class="col-md-6 text-center">
+                          <img src="imagenes/img004.png" class="imgTutorial">
+                      </div>
+                      <div class="col-md-6 text-center">
+                          <img src="imagenes/img005.png" class="imgTutorial">
+                      </div>
+                    </div>
+
+
+
+                  </ul>
+
+        </div>
+
+      </div>
+    </div>
+
+    <br><br><br>
+    <div class="row">
+      <div class="col-md-12 text-center">
+
+        <h3 class="tipografiaPrecursive3">DESCIFRAR TEXTO </h3>
+      </div>
+    </div>
+
+    <br>
+    <div class="row">
+      <div class="col-md-12 well">
+        <ul class="parrafo text-justify">
+            <li>HexaText descifrar&aacute cualquier texto previamente procesado con la misma aplicaci&oacuten, adem&aacutes de los textos almacenados en el m&oacutevil</li>
+          <li>Ingresar/copiar el texto en formato hexadecimal en el cuadro de texto</li>
+          <li>Ingresar la clave con la cual fue cifrado el texto</li>
+          <li>Pulsar en el bot&oacuten flotante inferior derecho y aparecer&aacute el texto descifrado</li><br>
+
+        </ul>
+        <div class="row">
+          <div class="col-md-6 text-center">
+              <img src="imagenes/img006.png" class="imgTutorial">&nbsp&nbsp&nbsp
+          </div>
+          <div class="col-md-6 text-center">
+              <img src="imagenes/img007.png" class="imgTutorial">
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </ul>
+
+
+  </div>
+
+
+
+</body>
+
+<?php include ("../secciones/pieApp.php"); ?>
+</html>
---
+++ secciones/pieApp.php
@@ -0,0 +1,33 @@
+<div class="seccionPie">
+  <div class="container">
+  <div class="row pieArriba">
+    <div class="col-md-6 col-sm-12 text-center ">
+          <a href="https://www.facebook.com/MOZAMAORG/" target="_blank" class="paddInterno">
+              <i class="fa fa-facebook-square fa-3x texto3d" aria-hidden="true"></i>
+          </a>
+
+          <div align="center" class="paddInterno">
+            <a href="" target="_blank" id="enlaceHuman">
+              <img class="img-responsive" id="imgHuman" src="../img/humanstxt-transparent-color.png" >
+            </a>
+          </div>
+
+    </div>
+    <div class="col-md-6 col-sm-12 text-center">
+      <p class="texto3d blanco paddInterno">
+        &copy; 2016 MOZAMA
+      </p>
+      <a href="mailto:contacto@mozama.com.mx"class="texto3d blanco paddInterno">
+        contacto@mozama.com.mx
+      </a>
+    </div>
+  </div>
+  <br><br>
+
+  </div>
+</div>
+
+<script type="text/javascript">
+  var dominio = window.location.host;
+  document.getElementById('enlaceHuman').href = dominio+"/humans.txt";
+</script>
