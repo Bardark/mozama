ebe14b9f24ee4280daf58a2b0b21d27da1c0b281
Bardark <ren_rbk@hotmail.es>
2017-04-23 19:28:18 -0500

    plugin face

--- index.php
+++ index.php
@@ -360,11 +360,7 @@
                  </p>
                </div>
              </div>
-             <div class="col-md-12 container">
-               <div align="center">
-                 <div class="fb-like" data-href="https://www.facebook.com/MOZAMAORG/" data-width="150" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
-               </div>
-             </div>
+               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMOZAMAORG%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
           </div>
 
         </section>
