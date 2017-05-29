3700982471198e5f59e9f008db749cd647e2eec4
asus <asus@asus-x.lan>
2017-05-28 22:02:40 -0500

    seguimiento google

--- index.php
+++ index.php
@@ -23,18 +23,17 @@
   <link rel="apple-touch-icon" sizes="152x152" href="img/apple-touch-icon-152x152-precomposed.png" />
 
 
-<!--
   <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-69468292-2', 'auto');
-  ga('send', 'pageview');
+    ga('create', 'UA-69468292-2', 'auto');
+    ga('send', 'pageview');
 
-</script>
--->
+  </script>
+  
 </head>
 
 <body data-spy="scroll" id="page-top">
